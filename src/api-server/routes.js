8384cc827444a435ad1c3f075d376bb7dfad3eac
Abid Rafi Nur Hastama <abidrafinurhastama@gmail.com>
2023-05-31 11:51:54 +0700

    add folder and files for api server

---
+++ src/api-server/routes.js
@@ -0,0 +1,29 @@
+const routes = [
+  {
+    method: 'POST',
+    path: '/contents',
+    handler: () => {}
+  },
+  {
+    method: 'GET',
+    path: '/contents',
+    handler: () => {}
+  },
+  {
+    method: 'GET',
+    path: '/contents/{id}',
+    handler: () => {}
+  },
+  {
+    method: 'PUT',
+    path: '/contents/{id}',
+    handler: () => {}
+  },
+  {
+    method: 'DELETE',
+    path: '/contents/{id}',
+    handler: () => {}
+  }
+]
+
+module.exports = routes
---
+++ src/api-server/server.js
@@ -0,0 +1,18 @@
+const Hapi = require('@hapi/hapi')
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*']
+      }
+    }
+  })
+
+  await server.start()
+  console.log(`Server berjalan pada ${server.info.uri}`)
+}
+
+init()
