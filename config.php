bb01a09cdb8ab549c2b262bcdaf0fc46f7aa36b4
Fakhri Setiawan <122422034+fakhrisetiawan@users.noreply.github.com>
2023-06-01 16:35:25 +0700

    koneksi ke db

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "equaliteach";
+
+try {    
+    $db = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+} catch(PDOException $e) {
+    die("Terjadi masalah: " . $e->getMessage());
+}
